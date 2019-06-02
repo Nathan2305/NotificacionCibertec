4d12601b81435dc1e20e766cead592625e00236b
Jonathan <nathan20092411@gmail.com>
2019-06-01 23:00:17 -0500

    Notificación cuando cambia la conectividad

--- notification/src/main/java/pe/cibertec/notification/MainActivity.java
+++ notification/src/main/java/pe/cibertec/notification/MainActivity.java
@@ -2,7 +2,10 @@ package pe.cibertec.notification;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
@@ -17,22 +20,29 @@ public class MainActivity extends AppCompatActivity {
         register();
         createNotificationChannel();
     }
+
     private void register() {
         MyBroadcastReceiver myBroadcastReceiver = new MyBroadcastReceiver();
         IntentFilter intentFilter = new IntentFilter();
-       // intentFilter.addAction("pe.cibertec.broadcast.ACTION");
-        intentFilter.addAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-       //intentFilter.addAction("android.intent.action.AIRPLANE_MODE");
+        // intentFilter.addAction("pe.cibertec.broadcast.ACTION");
+        // intentFilter.addAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        //intentFilter.addAction( Intent.ACTION_CAMERA_BUTTON);
+        //intentFilter.addAction("android.intent.action.AIRPLANE_MODE");
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+       // intentFilter.addAction(" android.intent.action.ACTION_CALL");
+        //intentFilter.addAction(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
         this.registerReceiver(myBroadcastReceiver, intentFilter);
     }
+
     private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            String name="pe.edu.notification.CHANNEL";
-            String description="Descripción";
-            int importance= NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel=new NotificationChannel("Canal",name,importance);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = "pe.edu.notification.CHANNEL";
+            String description = "Descripción";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Canal", name, importance);
             channel.setDescription(description);
-            NotificationManager notificationManager=getSystemService(NotificationManager.class);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
     }
--- notification/src/main/java/pe/cibertec/notification/MyBroadcastReceiver.java
+++ notification/src/main/java/pe/cibertec/notification/MyBroadcastReceiver.java
@@ -3,14 +3,18 @@ package pe.cibertec.notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
@@ -32,16 +36,17 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
         }*/
 
-        if (action.equalsIgnoreCase( Settings.ACTION_LOCATION_SOURCE_SETTINGS)){
+        if (action.equalsIgnoreCase( ConnectivityManager.CONNECTIVITY_ACTION)){
         //if (action.equalsIgnoreCase("android.intent.action.AIRPLANE_MODE")){
+       // if (action.equalsIgnoreCase("android.intent.action.ACTION_CALL")){
             Toast.makeText(context, "Acción ocurrió", Toast.LENGTH_SHORT).show();
             //CREAR NOTIFICACION
-            Intent newIntent=new Intent(context,NotificationActivity.class);
+               Intent newIntent=new Intent(context,NotificationActivity.class);
             PendingIntent pendingIntent=PendingIntent.getActivity(context,1,newIntent,0);
             NotificationCompat.Builder notification=new NotificationCompat.Builder(context,"Canal")
                     .setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentTitle("Notificacion GPS")
-                    .setContentText("GPS")
+                    .setContentTitle("Connectivity Changed")
+                    .setContentText("Connectivity")
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                     .setContentIntent(pendingIntent)
                     .setAutoCancel(true);
@@ -51,7 +56,8 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
             //Mostrar la notificacion
             NotificationManagerCompat managerCompat=NotificationManagerCompat.from(context);
-            managerCompat.notify(0,notification.build());
+            int random= new Random().nextInt()*10+1;
+            managerCompat.notify(random,notification.build());
         }
     }
 
